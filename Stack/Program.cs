34a2eb1fa68527e4c5d706312b5bf492863f1b0f
Jean Franco <93026071+jeanfrancodev@users.noreply.github.com>
2022-07-25 17:32:26 -0300

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using lista;
+using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esta é a Pilhaaa de livros!");
+
+            Stack<Palavra> livros = new Stack<Palavra>();
+            livros.Push(new Palavra("Limite Zero"));
+            livros.Push(new Palavra("Destrua este diário"));
+            livros.Push(new Palavra("tres porquinhos"));
+
+            livros.Peek();
+            livros.Pop();
+            Console.WriteLine($"O número de livros na Pilha é: {livros.Count}");
+
+            foreach (Palavra livro in livros) 
+            {
+                Console.WriteLine($"Título na nossa pilha: {livro}");
+            }
+
+        }
+    }
+}
