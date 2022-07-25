2e7244256e71fa256dbac89b1d2f535b8a6ef9e1
Jean Franco <93026071+jeanfrancodev@users.noreply.github.com>
2022-07-25 17:29:36 -0300

    Add files via upload

---
+++ List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de Animais!");
+            List<Palavra> Animais = new List<Palavra>();
+
+            Animais.Add(new Palavra("Cavalo"));
+            Animais.Add(new Palavra("Javali"));
+            Animais.Add(new Palavra("Ornitorrinco"));
+            Animais.Add(new Palavra("Galinha"));
+
+            Animais.Insert(2, new Palavra("Gato"));
+            Animais.Remove(new Palavra("Galinha"));
+            Animais.RemoveAt(1);
+
+            foreach (Palavra animal in Animais)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+
+    public class Palavra
+    {
+        public string Nome { get; set; }
+
+        public Palavra(string nome)
+        {
+            Nome = nome;
+        }
+        public override string ToString()
+        {
+            return Nome;
+        }
+
+    }
+
+}
+
+
