9d4d9d670657e5e35381030160addf4802f7d6de
Jean Franco <93026071+jeanfrancodev@users.noreply.github.com>
2022-07-25 17:16:56 -0300

    Add files via upload

---
+++ CDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace CDictionary 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Dicionario!");
+
+            Dictionary<string, Pessoa> dic = new Dictionary<string, Pessoa>();
+
+            dic.Add("Jepan", new Pessoa("Jean", "Guarulhos", 29));
+            dic.Add("Vó", new Pessoa("Avó", "Rio de Janeiro", 69));
+            dic.Add("Mainha", new Pessoa("Mãe", "Guarujá", 50));
+
+            foreach(KeyValuePair<string, Pessoa> keyValuePair in dic)
+            {
+                Console.WriteLine($"Chave = {keyValuePair.Key} | Valor =    {keyValuePai    r.Value}");      
+            }
+
+        }
+    }
+}
--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace fila
+namespace Queue
 {
-    class Program
+    class Program 
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World fila!");
+
+            Queue<Pessoa> pessoas = new Queue<Pessoa>();
+            pessoas.Enqueue(new Pessoa("João", "Los Santos", 12));
+            pessoas.Enqueue(new Pessoa("Maria", "São Paulo", 12));
+            pessoas.Enqueue(new Pessoa("Steve", "Ohio", 12));
+
+            Console.WriteLine($"Object primeiro da fila é: {pessoas.Count}");
+
+            pessoas.Peek();
+
+            Console.WriteLine($"O numero de pessoas na fila é: {pessoas.Count}");   
+
+            foreach(Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa.ToString());
+            }
+        }
+
+
+        public class Pessoa
+{
+            public string Nome { get; set; }
+            public string Cidade { get; set; }
+            public int Idade { get; set; }
+
+            public Pessoa(String nome, string cidade, int idade)
+            {
+                Nome = nome;
+                Cidade = cidade;
+                Idade = idade;
+            }
+            public override string ToString()
+            {
+                return $"Nome: {Nome} | Cidade: {Cidade} | Idade: {Idade}";
+            }
         }
     }
 }
+
